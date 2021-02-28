6f563bb01efcae90ec9b69744ef4d2e7f542c160
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2021-02-28 17:00:29 +0400

    создание тестовых методов вынесено в отдельный класс

---
+++ application.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Application:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def open_home_page(self):
+        self.driver.get("http://192.168.1.75/addressbook/")
+
+    def login(self, username, password):
+        self.open_home_page()
+        self.driver.set_window_size(945, 1020)
+        self.driver.find_element(By.NAME, "user").click()
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+    def open_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def create_group(self, group):
+        self.open_groups_page()
+        # init group creation
+        self.driver.find_element(By.NAME, "new").click()
+        # fill group form
+        self.driver.find_element(By.ID, "content").click()
+        self.driver.find_element(By.NAME, "group_name").click()
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.driver.find_element(By.NAME, "group_header").click()
+        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.driver.find_element(By.NAME, "group_footer").click()
+        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
+        self.driver.find_element(By.NAME, "submit").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "group page").click()
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def destroy(self):
+        self.driver.quit()
