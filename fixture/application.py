8ae2d80eec91ddd6c82a9b7054ab2043578a729c
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2021-03-18 21:01:50 +0400

    lesson 5_5 добавлена возможность запускать тесты под разными браузерами

--- fixture/application.py
+++ fixture/application.py
@@ -5,12 +5,17 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.vars = {}
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "firefox":
+            self.driver = webdriver.Firefox()
+        else:
+            raise ValueError("Unrecognised browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +26,7 @@ class Application:
 
     def open_home_page(self):
         driver = self.driver
-        driver.get("http://192.168.1.69/addressbook/")
+        driver.get(self.base_url)
 
     def destroy(self):
         driver = self.driver
